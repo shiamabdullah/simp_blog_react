7d22899a06b6a488224f853333be611395c03522
shiam <shiamabdullah1@gmail.com>
2021-10-08 02:54:12 +0600

    form creation

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+  const [author, setAuthor] = useState("mario");
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form action="">
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          value={title}
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label> Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Mario">mario</option>
+          <option value="Shiam">shiam</option>
+        </select>
+      </form>
+      <p>{title}</p>
+      <p>{body}</p>
     </div>
   );
 };
