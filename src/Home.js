d096313a2b5513e73dd007e68f595820f2961a79
shiam <shiamabdullah1@gmail.com>
2021-10-08 00:07:14 +0600

    custom error

--- src/Home.js
+++ src/Home.js
@@ -10,11 +10,18 @@ const Home = () => {
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        console.log(res);
+        if (!res.ok) {
+          throw Error("could not fetch data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
       });
   }, []);
 
