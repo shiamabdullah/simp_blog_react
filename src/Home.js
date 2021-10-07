13891eaa96bc3798f717a546616fc55ab0530c23
shiam <shiamabdullah1@gmail.com>
2021-10-07 20:20:52 +0600

    click events and buttons

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-function Home() {
+const Home = () => {
+  const handleClick = (e) => {
+    console.log("hello ninjas", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name + e.target);
+    console.log(e);
+  };
   return (
     <div className="home">
       <h2>HomePage</h2>
-      <button> Click Me</button>
+      <button onClick={handleClick}> Click Me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("mario", e);
+        }}
+      >
+        Click Me Again
+      </button>
     </div>
   );
-}
+};
 
 export default Home;
